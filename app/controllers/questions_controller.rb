ad6783678c7dcfa3613ce6584e69b3a689e10164
Chris Bourgeonnier <chrisbourgeonnier@gmail.com>
2023-11-06 13:48:30 +0900

    coach with style

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,11 +3,11 @@ class QuestionsController < ApplicationController
   end
 
   def answer
-    question = params["/answer"]
-    if question == "I am going to work"
-      @answer = "Great!"
-    elsif question.include?("?")
-      @answer = "Silly question, get dressed and go to work!"
+    @question = params["/answer"]
+    if @question == 'I am going to work'
+      @answer = 'Great!'
+    elsif @question.include?("?")
+      @answer = 'Silly question, get dressed and go to work!'
     else
       @answer = "I don't care, get dressed and go to work!"
     end
